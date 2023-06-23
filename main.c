16998ed0766024e476533df7531c5f92d451c608
chikhidev <chikhi.dev@gmail.com>
2023-06-22 17:30:51 -0700

    1

--- main.c
+++ main.c
@@ -8,39 +8,40 @@
 **/
 int main(int argc, char *argv[])
 {
-char *lineptr = NULL;
+char *tokens, *lineptr = NULL;
 unsigned int counter = 0;
 stack_t *head = NULL;
 ssize_t read_file;
 size_t bufsize;
 FILE *fl;
-char *tokens;
 
 if (argc != 2)
 {
 fprintf(stderr, "USAGE: monty file\n");
-return (EXIT_FAILURE);
+exit(EXIT_FAILURE);
 }
 
 fl = fopen(argv[1], "r");
 if (!fl)
 {
 fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-return (EXIT_FAILURE);
+exit(EXIT_FAILURE);
 }
 
 while ((read_file = _getline(&lineptr, &bufsize, fl)) != -1)
 {
-counter++;
 tokens = strtok(lineptr, LIMITER);
-if (!tokens)
+counter++;
+if (tokens)
+chooser(&head, tokens, counter);
+
+else if (!tokens)
 continue;
 
-chooser(&head, tokens, counter);
 }
-
+if (lineptr)
 free(lineptr);
 free_stack(&head);
 fclose(fl);
-return (EXIT_SUCCESS);
+exit(EXIT_SUCCESS);
 }
